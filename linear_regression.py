55ae84793a0eb2b2638fa8bf5cfde170c606e611
devshah21 <mail2devshah@gmail.com>
2023-09-03 20:11:04 -0400

    implemented plot, but running into errors

--- linear_regression.py
+++ linear_regression.py
@@ -22,4 +22,34 @@ def loss_function(m, b, points):
         y = points.iloc[i].score
         total_error += (y - (m*x + b)) **2 
     total_error / float(len(points))
+    
+def gradient_descent(m_now, b_now, points, learning_rate):
+    m_gradient = 0
+    b_gradient = 0
+    
+    n = len(points)
+    
+    for i in range(n):
+        x = points.iloc[i].studytime
+        y = points.iloc[i].score
+        m_gradient += -(2/n) * x * (y -(m_now * x + b_now))
+        b_gradient += -(2/n) * (y -(m_now * x + b_now))
+        
+    m = m_now - m_gradient * learning_rate
+    b = b_now - b_gradient * learning_rate
+    return m, b
+
+m = 0
+b = 0
+learning_rate = 0.0001
+epochs = 1000
+
+for i in range(epochs):
+    m, b = gradient_descent(m, b, data, learning_rate)
+
+print(m,b)
+
+plt.scatter(data.studytime, data.score, color='black')
+plt.plot(list(range(1, 10)), [m*x + b for x in range(1,10)], color='red')
+
         
